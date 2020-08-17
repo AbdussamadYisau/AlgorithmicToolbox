c750098fc59bb9b403204d7016b789d5d8d0d644
AbdussamadYisau <Sammieyisau@gmail.com>
2020-08-17 15:14:06 +0100

    Added efficient solution for fibonacci number

--- week2_algorithmic_warmup/1_fibonacci_number/fibonacci.py
+++ week2_algorithmic_warmup/1_fibonacci_number/fibonacci.py
@@ -1,9 +1,26 @@
 # Uses python3
+
+
+# Naive Solution
+# def calc_fib(n):
+#     if (n <= 1):
+#         return n
+
+#     return calc_fib(n - 1) + calc_fib(n - 2)
+
+# Efficient solution
+
+fibArray = [0, 1]
 def calc_fib(n):
-    if (n <= 1):
+
+    if n <= 1:
         return n
 
-    return calc_fib(n - 1) + calc_fib(n - 2)
+    for i in range(n):
+        fibArray[i-1], fibArray[i] = fibArray[i], fibArray[i-1] + fibArray[i] 
+        fibArray.append(fibArray[i])
+
+    return fibArray[-1]
 
 n = int(input())
 print(calc_fib(n))
